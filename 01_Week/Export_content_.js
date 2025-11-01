dcc0c385fae9843a1ffdaa267fd0dafb9ab9170e
Mohd-Amer-2957 <mohdamer2957@gmail.com>
2025-11-02 01:46:00 +0530

    1 Week Learning

---
+++ 01_Week/Export_content_.js
@@ -0,0 +1,5 @@
+const naam="amer khan";
+ export const arr=[2,4,6,8];       // named export
+
+
+export default naam;     // exporting to another js file            // default export
---
+++ 01_Week/simple.js
@@ -0,0 +1,6 @@
+const users = ["Aman", "Zoya", "Ravi"];
+const greetings = users.map(name => `Hello, ${name}!`);
+console.log(greetings);
+// ["Hello, Aman!", "Hello, Zoya!", "Hello, Ravi!"]
+
+
